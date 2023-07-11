8408ee675c1fe23bc63905311fe754d73ee2dbdc
subodhpoudel12 <subodhpoudel1000@gmail.com>
2023-07-11 19:39:56 +0545

    nav bar design

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React from 'react'
 import './App.css'
 import { Footer, Blog, Possibility, Features, whatgpt3,Header } from './containers'
 import { Cta,Brand, Navbar  } from './components'
+import Whatgpt3 from './containers/whatGPT3/Whatgpt3'
 const App = () => {
   return (
-    <div>
-      <h1>New project</h1>
+    <div className='App'>
+    <div className='gradient__bg'>
+      <Navbar/>
+      <Header/>
+    </div>
+    <Brand/>
+    <Whatgpt3/>
+    <Features/>
+    <Possibility/>
+    <Cta/>
+    <Blog/>
+    <Footer/> 
     </div>
   )
 }
